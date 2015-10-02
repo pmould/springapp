051bbca34ac3c3583ca1732cbd60101c8145ca78
Paul Mould <paul_mould@hotmail.com>
2015-10-02 16:41:54 -0500

    First Commit

---
+++ src/main/java/com/beingjavaguys/test/jdbc.java
@@ -0,0 +1,46 @@
+package com.beingjavaguys.test;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class jdbc {
+	public static void main(String[] args)
+	{
+		
+		System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Where is your MySQL JDBC Driver?");
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println("MySQL JDBC Driver Registered!");
+		Connection connection = null;
+
+		try {
+			connection = DriverManager
+			.getConnection("jdbc:mysql://rsi-tutorial.cf3w7n2kxlkf.us-west-2.rds.amazonaws.com:3306/springhibernate_db","paul_mould", "ruralsourcing");
+
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}	
+		
+	}
+}
